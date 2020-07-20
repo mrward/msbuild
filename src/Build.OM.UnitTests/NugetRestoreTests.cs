bcce20279cf36cf8dfdba96e2236fb8b76ff4628
Ankit Jain <ankit.jain@microsoft.com>
2020-07-20 10:54:47 -0400

    [mono][tests] Disable TestOldNuGet test

--- src/Build.OM.UnitTests/NugetRestoreTests.cs
+++ src/Build.OM.UnitTests/NugetRestoreTests.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Build.Engine.OM.UnitTests
         // This NuGet version cannot locate other assemblies when parsing solutions at restore time. This includes localized strings required in debug mode.
         // NuGet version 4.1.0 was somewhat arbitrarily chosen. 3.5 breaks with an unrelated error, and 4.8.2 does not fail when a new dependency is introduced. This is a safe middle point.
 #if !DEBUG
-        [SkipOnTargetFramework(TargetFrameworkMonikers.Netcoreapp)]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.Netcoreapp | TargetFrameworkMonikers.Mono)]
         [Fact]
         public void TestOldNuget()
         {
