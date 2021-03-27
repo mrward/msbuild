d094b49f5b5095efe15685431275b9c38fa6d845
Alexander Köplinger <alex.koeplinger@outlook.com>
2021-03-27 13:05:26 +0100

    Add binding redirects for System.Reflection.Metadata
    This is necessary to ensure we use the same SRM as Roslyn

--- src/MSBuild/GitBuildInfo.cs
+++ src/MSBuild/GitBuildInfo.cs
@@ -1,1 +1,1 @@
-class GitBuildInfoForMono { public static string BuildInfo = "xplat-master/4b275b319 Sat Jul 18 20:07:30 EDT 2020"; }
+class GitBuildInfoForMono { public static string BuildInfo = "xplat-master/9acaaeee4 Sat Mar 27 12:09:15 CET 2021"; }
